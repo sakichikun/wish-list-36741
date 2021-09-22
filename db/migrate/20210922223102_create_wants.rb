281d5aff2619a32ad183162fb08d2824cdc06fab
sakichikun <sakitty24.xxx@gmail.com>
2021-09-23 07:53:58 +0900

    wantsテーブルの作成

--- db/migrate/20210922223102_create_wants.rb
+++ db/migrate/20210922223102_create_wants.rb
@@ -1,7 +1,8 @@
 class CreateWants < ActiveRecord::Migration[6.0]
   def change
     create_table :wants do |t|
-
+      t.string :name, null: false
+      t.string :title, null: false
       t.timestamps
     end
   end
