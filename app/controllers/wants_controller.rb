d18db51341c32e90817c22b612e837ef405dc6e9
sakichikun <sakitty24.xxx@gmail.com>
2021-09-23 13:40:52 +0900

    投稿完了後の処理設定

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -4,6 +4,21 @@ class WantsController < ApplicationController
   end
 
   def new
-    @wants = Want.new
+    @want = Want.new
+  end
+
+  def create
+    @want = Want.new(want_params)
+    if @want.valid?
+      @want.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def want_params
+    params.require(:want).permit(:name, :title)
   end
 end
--- app/models/want.rb
+++ app/models/want.rb
@@ -1,2 +1,4 @@
 class Want < ApplicationRecord
+  validates :name, presence: true
+  validates :title, presence: true
 end
