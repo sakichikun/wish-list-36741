c80665a22ef6c268de11fc80094865bf2f00c208
sakichikun <sakitty24.xxx@gmail.com>
2021-09-24 23:02:00 +0900

    wishテーブルnameカラムの削除

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,5 +1,4 @@
 class Wish < ApplicationRecord
-  validates :name, presence: true
   validates :title, presence: true
 
   belongs_to :user
--- db/migrate/20210924090043_create_wishes.rb
+++ db/migrate/20210924090043_create_wishes.rb
@@ -1,7 +1,6 @@
 class CreateWishes < ActiveRecord::Migration[6.0]
   def change
     create_table :wishes do |t|
-      t.string :name, null: false
       t.string :title, null: false
       t.references :user,  null: false, foreign_key: true
       t.timestamps
