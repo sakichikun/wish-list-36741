032d385673405652f6cd480ba0d60109c08476ed
sakichikun <sakitty24.xxx@gmail.com>
2021-09-25 11:19:54 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'wishes#index'
-  resources :wishes
+  resources :wishes do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
