2331ff8ee4c09557ebb86f0da3023ce39a2f492e
sakichikun <sakitty24.xxx@gmail.com>
2021-09-25 11:15:52 +0900

    アソシエーションの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   has_many :wishes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -2,4 +2,5 @@ class Wish < ApplicationRecord
   validates :title, presence: true
 
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
