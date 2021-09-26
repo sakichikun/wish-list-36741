9fb8a2a125b97e65046f8cb7a655218f8fbbf2ff
sakichikun <sakitty24.xxx@gmail.com>
2021-09-26 23:39:06 +0900

    jqueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 //= require jquery
+//= require jquery_ujs
 require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
