77bd5109728e79ca507cd8264a73f3f441fbc8c8
sakichikun <sakitty24.xxx@gmail.com>
2021-09-29 13:08:06 +0900

    画像アップロード機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,8 @@ class CommentsController < ApplicationController
     redirect_to action: :index, controller: 'wishes'
   end
 
+  
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, wish_id: params[:wish_id])
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -8,6 +8,7 @@ class Wish < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_one_attached :image
 
   def self.search(search)
     if search != ""
