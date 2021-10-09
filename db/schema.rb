780b8c018202694fcf1b12e8e49079e549a13399
sakichikun <sakitty24.xxx@gmail.com>
2021-10-09 12:06:50 +0900

    introductionカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_132045) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
+    t.text "introduction"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
