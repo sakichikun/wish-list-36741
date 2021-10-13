a6ab9ae907f7667b503bb2899756b4db58108e21
sakichikun <sakitty24.xxx@gmail.com>
2021-10-13 20:30:04 +0900

    不要なコードの削除

--- app/javascript/popup.js
+++ app/javascript/popup.js
@@ -2,11 +2,9 @@ function popupImage() {
   const popup = document.getElementById('js-popup');
   if(!popup) return;
 
-  const blackBg = document.getElementById('js-black-bg');
   const closeBtn = document.getElementById('js-close-btn');
   const showBtn = document.getElementById('js-show-popup');
 
-  closePopUp(blackBg);
   closePopUp(closeBtn);
   closePopUp(showBtn);
   function closePopUp(elem) {
