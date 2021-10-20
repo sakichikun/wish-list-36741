09d17f11c53f6f574c6ba2ac80bf23a7edb37956
sakichikun <sakitty24.xxx@gmail.com>
2021-10-20 17:09:22 +0900

    wishモデルのテストコードを追加

--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe Wish, type: :model do
 
   describe 'Bucket Listの保存' do
     context 'Bucket Listが投稿できる場合' do
-      it 'titleを投稿できる' do
+      it 'title,due_time,state_id,imageが存在していれば投稿できる' do
+        expect(@wish).to be_valid
+      end
+
+      it 'imageが空でも投稿できる' do
+        @wish.image = nil
         expect(@wish).to be_valid
       end
     end
@@ -17,6 +22,18 @@ RSpec.describe Wish, type: :model do
         @wish.title = ''
         @wish.valid?
         expect(@wish.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'due_timeが空では投稿できない' do
+        @wish.due_time = ''
+        @wish.valid?
+        expect(@wish.errors.full_messages).to include("Due time can't be blank")
+      end
+
+      it 'state_idに1が選択されている場合は投稿できない' do
+        @wish.state_id = '1'
+        @wish.valid?
+        expect(@wish.errors.full_messages).to include("State can't be blank")
       end
 
       it 'userが紐付いていなければ投稿できない' do
