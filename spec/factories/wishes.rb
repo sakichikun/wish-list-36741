2a11b0af2bc636bbda1980c7d99f4b976455b7f8
sakichikun <sakitty24.xxx@gmail.com>
2021-10-20 16:37:09 +0900

    wishモデルにFakerを追加

--- spec/factories/wishes.rb
+++ spec/factories/wishes.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :wish do
     title {Faker::Lorem.sentence}
+    due_time {2021-10-24}
+    state_id {2}
     association :user
+
+    after(:build) do |wish|
+      wish.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.png')
+    end
   end
 end
